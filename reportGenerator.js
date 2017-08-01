84dfdfff1cc34c48f9a00e8c2f5a616df2006483
Davide Texiera <Davidepaalte@users.noreply.github.com>
2017-08-01 15:39:05 +0100

    Update reportGenerator.js
    Report functional. Flags are not detected yet.

--- reportGenerator.js
+++ reportGenerator.js
@@ -12,6 +12,60 @@ const PALADIN = "knight";
 const NOBLE = "snob";
 const MILITIA = "militia";
 
+var report = window.open ("https://" + window.location.host + "/game.php?screen=place&mode=sim","report");
+report.onload = function() { fillReport(report); };
+
+function fillReport(report) {
+  // Attacker
+  fillReportByUnit(report, SPEARMAN, "Atk");
+  fillReportByUnit(report, SWORDSMAN, "Atk");
+  fillReportByUnit(report, AXEMAN, "Atk");
+  fillReportByUnit(report, ACHER, "Atk");
+  fillReportByUnit(report, SCOUT, "Atk");
+  fillReportByUnit(report, LIGHT_CAVALRY, "Atk");
+  fillReportByUnit(report, MOUNTED_ARCHER, "Atk");
+  fillReportByUnit(report, HEAVY_CAVALRY, "Atk");
+  fillReportByUnit(report, RAM, "Atk");
+  fillReportByUnit(report, CATAPULT, "Atk");
+  fillReportByUnit(report, PALADIN, "Atk");
+  fillReportByUnit(report, NOBLE, "Atk");
+  
+  // Defender
+  fillReportByUnit(report, SPEARMAN, "Def");
+  fillReportByUnit(report, SWORDSMAN, "Def");
+  fillReportByUnit(report, AXEMAN, "Def");
+  fillReportByUnit(report, ACHER, "Def");
+  fillReportByUnit(report, SCOUT, "Def");
+  fillReportByUnit(report, LIGHT_CAVALRY, "Def");
+  fillReportByUnit(report, MOUNTED_ARCHER, "Def");
+  fillReportByUnit(report, HEAVY_CAVALRY, "Def");
+  fillReportByUnit(report, RAM, "Def");
+  fillReportByUnit(report, CATAPULT, "Def");
+  fillReportByUnit(report, PALADIN, "Def");
+  fillReportByUnit(report, NOBLE, "Def");
+  fillReportByUnit(report, MILITIA, "Def");
+  
+  // Defender wall
+  report.document.getElementsByName("def_wall")[0].value = getWallLvl();
+  
+  // Religion
+  report.document.getElementsByName("belief_att")[0].checked =
+    ($("#attack_info_att").find('td:contains("The troops were religious.")').length === 1);
+  report.document.getElementsByName("belief_def")[0].checked =
+    ($("#attack_info_def").find('td:contains("The troops were religious.")').length === 1);
+}
+
+function getWallLvl () {
+  return $("#attack_spy_buildings_right>tbody>tr").find('td:contains(Wall)').parent().children()[1].innerHTML;
+}
+
+function fillReportByUnit(report, type, side) {
+  if (side === "Atk")
+    report.document.getElementsByName("att_"+type)[0].value = getTroopCount(type, "Atk");
+  else
+    report.document.getElementsByName("def_"+type)[0].value = getTroopCount(type, "Def");
+}
+
 function getTroopCount(type, side, aliveTroops = true) {  
   switch (type) {
     case "spear":
@@ -48,7 +102,7 @@ function getTroopCount(type, side, aliveTroops = true) {
 function getTroopCountByID(id, side, aliveTroops = true) {
   if (id === ".unit-item.unit-item-militia") {
     if (aliveTroops)
-      return $(id)[0].innerHTML - return $(id)[1].innerHTML;
+      return $(id)[0].innerHTML - $(id)[1].innerHTML;
     
     return $(id)[0].innerHTML;
   }
